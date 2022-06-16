4c1559fdc80c4dfede389c7924d0ba18fcbe7b51
wangzeping <wangzeping.wh@haier.com>
2022-06-16 15:26:33 +0800

    Revert "Revert "change""
    This reverts commit 60d9cec2f6b521888b999e5d05cfa7bfaae04d6e.

--- lib/wrapper_plugins/janus_video_room_plugin.dart
+++ lib/wrapper_plugins/janus_video_room_plugin.dart
@@ -129,12 +129,15 @@ class JanusVideoRoomPlugin extends JanusPlugin {
     await this.send(data: payload);
   }
 
-  Future<void> subscribeToStreams(List<PublisherStream> streams) async {
+  Future<void> subscribeToStreams(List<PublisherStream> streams,feed) async {
     if (streams.length > 0) {
       var payload = {
         'request': "subscribe",
+        "feed": feed,
         'streams': streams.map((e) => e.toMap()).toList()
       };
+      this.feedId = feed.toString();
+      super.feedId = feedId.toString();
       await this.send(data: payload);
     }
   }
